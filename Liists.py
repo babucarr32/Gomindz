4a3d162f22ebad0eb7fe0e8d829aa3cd002ef961
babucarr32 <baboucarry@gmail.com>
2022-05-31 18:46:54 +0000

    GOmindz academy

--- Liists.py
+++ Liists.py
@@ -1,2 +1,7 @@
-List = [1, 1, 1, ["a", "b"]]
-print(List[3][1])
+# List = ["a","g","r","c"]
+# # List.append("Baba")
+# # List.insert(2, "Mam")
+# # print(List.count(1))
+# # List.pop(2)
+# # print(List[:])
+# print(sorted(List))
