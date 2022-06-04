3c92632f69a8830fe658f2c22bfe3120ce3b6def
babucarr32 <baboucarry@gmail.com>
2022-06-04 09:36:20 +0000

    Working on my LisTryMe

---
+++ ListTryMe.py
@@ -0,0 +1,26 @@
+# #Question 1
+# names = ["Alieu", "Abubacar", "Mahmood", "Mariama", "Jawo",
+# "Ebrima", "Jainaba"]
+# print(names[0:3])
+
+# # Question 2
+# names = ["sara", "chanel", "mitch", "ryan", "holy", "alex", "rob"]
+# print(names)
+# print(names[1:4])
+# print(names[3:])
+
+# # QUestion 3
+# my_list = ["Mitch", ["sara", "sally", "joe"], "peter", "aly"]
+# print(my_list[1], [1])
+# print(my_list[1])
+# print(my_list[-1])
+
+# # Question 4
+# my_list = ["Mitch", ["sara", "sally",
+# "joe"], "peter", "aly"]
+
+import random
+
+names = ['Babucarr', "Modou", "Abdou"]
+choice = random.choice(names)
+print(f"{choice} will make the payments")
