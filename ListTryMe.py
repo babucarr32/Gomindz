48d990cd1064406aecf4fe8019e89b6191828c6c
babucarr32 <baboucarry@gmail.com>
2022-06-04 11:47:31 +0000

    Workin on my ListTryMe

--- ListTryMe.py
+++ ListTryMe.py
@@ -19,8 +19,34 @@
 # my_list = ["Mitch", ["sara", "sally",
 # "joe"], "peter", "aly"]
 
-import random
+# Question 5
 
-names = ['Babucarr', "Modou", "Abdou"]
-choice = random.choice(names)
-print(f"{choice} will make the payments")
+# my_list = ["Mitch", ["sara", "sally",
+# "joe"], "peter", "aly"]
+# my_list.remove("peter")
+# print(my_list)
+
+# my_list = ["Mitch", ["sara", "sally",
+# "joe"], "peter", "aly"]
+# del my_list[2]
+# print(my_list)
+
+# Question 6
+# my_list = [50, 20, 30, 10, 40, 15, 4]
+# sorted_liist = sorted(my_list)
+# sorted_liist.reverse()
+# print(sorted_liist)
+
+# Question 7
+# grocery_list = [['chips','jelly','chocolate'],['sweet potatoes','potatoes'],['peanuts','protein bar']]
+# lastList = grocery_list[-1]
+# print(lastList)
+
+# Question 8
+# Friends = ["Baboucarr", "Mustapha", "Ansumana", "Cherno", "Yunusa"]
+# Friends.sort()
+# print(Friends)
+# import random
+# names = ['Babucarr', "Modou", "Abdou"]
+# choice = random.choice(names)
+# print(f"{choice} will make the payments")
