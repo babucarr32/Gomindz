e87f518c688113f2f798a3458fa02b5b828968ae
babucarr32 <baboucarry@gmail.com>
2022-06-11 11:02:53 +0000

    Working with sets .update

--- MySets.py
+++ MySets.py
@@ -2,7 +2,11 @@ mySet = {1, 2, 6, 4, 3, 8}
 myNewSet = {2, 5, 10, 5, 6, 8, 2, 10}
 # print(mySet)
 # print(myNewSet)
-print(10 in myNewSet)
-print(dir(myNewSet))
-print(myNewSet.intersection(mySet))
+# print(10 in myNewSet)
+# print(dir(myNewSet))
+# print(myNewSet.intersection(mySet))
 
+myNewSet.add('sundiata')
+myNewSet.update('mustapha')
+
+print(myNewSet)
