51dfe52cdf42726e1231f5c470a97535b6e2fe44
babucarr32 <baboucarry@gmail.com>
2022-06-11 10:56:29 +0000

    Working with sets

---
+++ MySets.py
@@ -0,0 +1,8 @@
+mySet = {1, 2, 6, 4, 3, 8}
+myNewSet = {2, 5, 10, 5, 6, 8, 2, 10}
+# print(mySet)
+# print(myNewSet)
+print(10 in myNewSet)
+print(dir(myNewSet))
+print(myNewSet.intersection(mySet))
+
