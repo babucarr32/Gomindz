dcee08736c5bca7b819ef919a96df51d23d35c59
babucarr32 <baboucarry@gmail.com>
2022-06-18 23:26:10 +0000

    Gomindzest

---
+++ GomindzTest.py
@@ -0,0 +1,47 @@
+## Question 1
+
+# bodyMass = float(input('Enter body mass: '))
+# weight = float(input('Enter weight: '))
+# BMI = bodyMass / weight ** 2
+#
+# print(BMI)
+# if BMI < 18.5:
+#     print('You are underweight.')
+# elif BMI > 18.5 and BMI < 25:
+#     print("You have normal weight")
+# elif BMI > 25 and BMI < 30:
+#     print('You are slightly overweight')
+# elif BMI > 30 and BMI < 35:
+#     print('You are obese')
+# else:
+#     print('You are clinically obese')
+
+
+## Question 2
+# height = int(input('Whats your height? '))
+# if height < 120:
+#     print('You cant ride')
+# elif height > 120:
+#     print('You can ride')
+#     age = int(input("What's receive age? "))
+#     if age < 12:
+#         print('You will have $5')
+#     elif age > 12 and age < 18:
+#         print('You will receive $7')
+#     else:
+#         print('You will receive $12')
+
+
+## Question 3
+
+# number = int(input('Enter a number: '))
+# if number % 4 != 0:
+#     print('Not Leap')
+# else:
+#     if number % 100 != 0:
+#         print('Leap')
+#     else:
+#         if number % 400 != 0:
+#             print('Not Leap')
+#         else:
+#             print('Leap')
